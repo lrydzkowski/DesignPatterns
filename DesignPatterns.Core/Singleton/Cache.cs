5cff6898c7de83d55a6ba96e35c8f0923e9d0721
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2022-01-29 10:50:02 +0100

    feat: Singleton example

---
+++ DesignPatterns.Core/Singleton/Cache.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace DesignPatterns.Core.Singleton;
+
+internal class Cache
+{
+    private static int instanceCount = 0;
+
+    private static object cacheLock = new object();
+
+    private Cache() 
+    {
+        
+    }
+
+    public static IMessage? Message { get; set; } = null;
+
+    private static Cache? instance = null;
+    public static Cache Instance
+    {
+        get
+        {
+            lock (cacheLock)
+            {
+                if (instance == null)
+                {
+                    Message?.Write($"New cache object is creating ({instanceCount++})...");
+                    instance = new Cache();
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ DesignPatterns.Core/Singleton/Executor.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Core.Singleton;
+
+internal class Executor : IExecutor
+{
+    public Executor(IMessage message)
+    {
+        Message = message;
+    }
+
+    public IMessage Message { get; }
+
+    public void Execute()
+    {
+        Cache.Message = Message;
+        List<Task> tasks = new();
+        for (int i = 0; i < 25; i++)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                Cache cache = Cache.Instance;
+            }));
+        }
+        Task.WaitAll(tasks.ToArray());
+    }
+}
