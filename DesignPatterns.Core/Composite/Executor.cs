ffe6e71e0759b15bf1ad93db768e9d8c76c46896
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2022-01-30 10:46:37 +0100

    feat: Composite example

---
+++ DesignPatterns.Core/Composite/Executor.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.Core.Composite;
+
+internal class Executor : IExecutor
+{
+    public Executor(IMessage message)
+    {
+        Message = message;
+    }
+
+    public IMessage Message { get; }
+
+    public void Execute()
+    {
+        Node secondLvl1 = new("secondLvl1", Message);
+        Node secondLvl2 = new("secondLvl2", Message);
+        Node firstLvl1 = new("firstLvl1", Message);
+        firstLvl1.Children.Add(secondLvl1);
+        firstLvl1.Children.Add(secondLvl2);
+
+        Node secondLvl3 = new("secondLvl3", Message);
+        Node firstLvl2 = new("firstLvl2", Message);
+        firstLvl2.Children.Add(secondLvl3);
+
+        Node rootNode = new("rootNode", Message);
+        rootNode.Children.Add(firstLvl1);
+        rootNode.Children.Add(firstLvl2);
+
+        rootNode.DisplayName();
+    }
+}
---
+++ DesignPatterns.Core/Composite/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DesignPatterns.Core.Composite;
+
+internal class Node
+{
+    public Node(string name, IMessage message)
+    {
+        Name = name;
+        Message = message;
+    }
+
+    public string Name { get; }
+    public IMessage Message { get; }
+    public List<Node> Children { get; private set; } = new();
+
+    public void DisplayName(int indentation = 0)
+    {
+        Message.Write(new string(' ', indentation) + Name);
+        foreach (Node child in Children)
+        {
+            child.DisplayName(indentation + 4);
+        }
+    }
+}
